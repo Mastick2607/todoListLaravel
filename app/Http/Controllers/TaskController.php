dc1fa84907b64c2eab898f971d72bbd448930e9d
marlon <mstick2607@gmail.com>
2024-12-11 21:16:19 -0500

    subir

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tasks['tasks'] = Task::all();
+        return view('task.index',$tasks);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $task = request()->all();
+
+        Task :: create($task);
+        return redirect('/a');
+        // print_r($_POST);
+        // echo 'swjshjshjs';
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function show(task $task)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(task $task)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, task $task)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $task =Task::findOrFail($id);
+        $task->delete();
+        return redirect('/a');
+    }
+}
